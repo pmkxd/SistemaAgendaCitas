6502e57c1a5bbe14b1d0edc595c286442650eab9
Rodolfo Gallardo <rodolfo7gallardo@gmail.com>
2025-05-28 19:58:50 -0400

    Scaffonding, controladores, vistas, layout

---
+++ SistemaAgendaCitas/Controllers/CitasController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SistemaAgendaCitas.Data;
+using SistemaAgendaCitas.Models.Entities;
+
+namespace SistemaAgendaCitas.Controllers
+{
+    public class CitasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CitasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Citas
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Citas.Include(c => c.Cliente).Include(c => c.Servicio);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Citas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas
+                .Include(c => c.Cliente)
+                .Include(c => c.Servicio)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            return View(cita);
+        }
+
+        // GET: Citas/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Nombre");
+            ViewData["ServicioId"] = new SelectList(_context.Servicios, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Citas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ClienteId,ServicioId,Fecha,Hora,Estado,Comentarios")] Cita cita)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cita);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Si hay error, recargar los combos
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Nombre", cita.ClienteId);
+            ViewData["ServicioId"] = new SelectList(_context.Servicios, "Id", "Nombre", cita.ServicioId);
+            return View(cita);
+        }
+
+        // GET: Citas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas.FindAsync(id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Apellido", cita.ClienteId);
+            ViewData["ServicioId"] = new SelectList(_context.Servicios, "Id", "Descripcion", cita.ServicioId);
+            return View(cita);
+        }
+
+        // POST: Citas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ClienteId,ServicioId,Fecha,Hora,Estado,Comentarios")] Cita cita)
+        {
+            if (id != cita.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cita);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CitaExists(cita.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Apellido", cita.ClienteId);
+            ViewData["ServicioId"] = new SelectList(_context.Servicios, "Id", "Descripcion", cita.ServicioId);
+            return View(cita);
+        }
+
+        // GET: Citas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas
+                .Include(c => c.Cliente)
+                .Include(c => c.Servicio)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            return View(cita);
+        }
+
+        // POST: Citas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cita = await _context.Citas.FindAsync(id);
+            if (cita != null)
+            {
+                _context.Citas.Remove(cita);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CitaExists(int id)
+        {
+            return _context.Citas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SistemaAgendaCitas/Controllers/ServiciosController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SistemaAgendaCitas.Data;
+using SistemaAgendaCitas.Models.Entities;
+
+namespace SistemaAgendaCitas.Controllers
+{
+    public class ServiciosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServiciosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Servicios
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Servicios.ToListAsync());
+        }
+
+        // GET: Servicios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            return View(servicio);
+        }
+
+        // GET: Servicios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Servicios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Descripcion,Duracion,Precio,Activo")] Servicio servicio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(servicio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servicio);
+        }
+
+        // GET: Servicios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios.FindAsync(id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+            return View(servicio);
+        }
+
+        // POST: Servicios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Descripcion,Duracion,Precio,Activo")] Servicio servicio)
+        {
+            if (id != servicio.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(servicio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServicioExists(servicio.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servicio);
+        }
+
+        // GET: Servicios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            return View(servicio);
+        }
+
+        // POST: Servicios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var servicio = await _context.Servicios.FindAsync(id);
+            if (servicio != null)
+            {
+                _context.Servicios.Remove(servicio);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServicioExists(int id)
+        {
+            return _context.Servicios.Any(e => e.Id == id);
+        }
+    }
+}
