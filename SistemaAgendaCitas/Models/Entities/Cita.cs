becafa14a8379a6b08938d1c2095b8fc3b56ebf1
Rodolfo Gallardo <rodolfo7gallardo@gmail.com>
2025-05-28 18:58:03 -0400

    agregue validaciones a las entidades cita y cliente

---
+++ SistemaAgendaCitas/Models/Entities/Cita.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaAgendaCitas.Models.Entities
+{
+    public class Cita
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Cliente es requerido.")]
+        public int ClienteId { get; set; }
+
+        public Cliente Cliente { get; set; } = null!;
+
+        [Required(ErrorMessage = "Servicio es requerido.")]
+        public int ServicioId { get; set; }
+
+        public Servicio Servicio { get; set; } = null!;
+
+        [Required(ErrorMessage = "Fecha es requerida.")]
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "Hora es requerida.")]
+        [DataType(DataType.Time)]
+        public TimeSpan Hora { get; set; }
+
+        [Required(ErrorMessage = "Estado es requerido.")]
+        [StringLength(20, ErrorMessage = "El estado debe tener un máximo de 20 caracteres.")]
+        public string Estado { get; set; } = "Pendiente";
+
+        [StringLength(250, ErrorMessage = "Los comentarios no deben superar los 250 caracteres.")]
+        public string? Comentarios { get; set; }
+    }
+}
