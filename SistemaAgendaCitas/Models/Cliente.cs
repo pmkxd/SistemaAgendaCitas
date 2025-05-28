36fc7024333e5ec104f217a4e7a0d7b9d9273256
Rodolfo Gallardo <rodolfo7gallardo@gmail.com>
2025-05-28 18:31:12 -0400

    Agregue: dependencias y actualice a la version 9.0.0 modelo cliente

---
+++ SistemaAgendaCitas/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace SistemaAgendaCitas.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; } // Clave primaria
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Telefono { get; set; } = null!;
+        public DateTime FechaRegistro { get; set; }
+
+        // Relaciones
+        public ICollection<Cita> Citas { get; set; } = new List<Cita>();
+    }
+}
