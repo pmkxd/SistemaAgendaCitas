c6b7eb9d2c579fc1b8e00da5bce2218fd4c198cb
Rodolfo Gallardo <rodolfo7gallardo@gmail.com>
2025-05-31 15:40:13 -0400

    Incluir filtros por fecha, cliente y estado Mostrar información detallada (cliente, servicio, fecha, hora, estado) Validar disponibilidad de horario (evitar solapamientos)Permitir cambiar fecha, hora y estado Implementar funcionalidad para cambiar el estado de una cita Permitir cancelar citas pendientes Permitir marcar citas como completadas Registrar la fecha y hora del cambio de estado

---
+++ SistemaAgendaCitas/Models/ViewModels/EditCitaViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaAgendaCitas.Models.ViewModels
+{
+    public class EditCitaViewModel
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeSpan Hora { get; set; }
+
+        [Required]
+        public EstadoCita Estado { get; set; }
+
+        public string Comentarios { get; set; } = "";
+
+        public string ClienteNombre { get; set; } = "";
+
+        public string ServicioNombre { get; set; } = "";
+
+    }
+
+}
