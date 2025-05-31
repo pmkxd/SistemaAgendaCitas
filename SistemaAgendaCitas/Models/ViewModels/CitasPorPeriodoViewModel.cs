3ca4ccf0197f5e33eb7d0ca399884be377882ec0
Pedro <pmkxd110@gmail.com>
2025-05-30 23:13:32 -0400

    Reportes por periodo

---
+++ SistemaAgendaCitas/Models/ViewModels/CitasPorPeriodoViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaAgendaCitas.Models.ViewModels
+{
+    public class CitasPorPeriodoViewModel
+    {
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public int Pendientes { get; set; }
+        public int Confirmadas { get; set; }
+        public int Completadas { get; set; }
+        public int Canceladas { get; set; }
+    }
+}
